bd4180da727bf03e8132db18b0982e487ebfdd34
anakarinavarro <anakarina.stratus@gmail.com>
2022-08-22 14:33:01 -0400

    TOdo Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,62 @@
 
 import React from "react";
-import shortid from "shortid";
+import { nanoid } from 'nanoid';
 
 function App() {
 
 const [tarea, setTarea] = React.useState("")
 const [tareas, setTareas] = React.useState([])
+const [modoEdicion, setModoEdicion] = React.useState(false)
+const [id, setId] = React.useState("")
+const [error, setError] = React.useState(null)
+
 const agregarTarea = e =>{
   e.preventDefault()
   if(!tarea.trim()){
     console.log("el campo esta vacio")
+    setError("Escriba una tarea")
     return
   }
   console.log(tarea)
   setTareas([
     ...tareas,
-    {id: shortid.generate,nombreTarea: tarea}
+    {id: nanoid(10),nombreTarea: tarea}
   ])
 
   setTarea("")
+  setError(null)
   
 }
+
+const eliminarTarea = id=> {
+  const arrayFiltrado = tareas.filter(item => item.id !== id)
+  setTareas(arrayFiltrado)
+
+}
+
+const editar = item => {
+  console.log(item)
+  setModoEdicion(true)
+  setTarea(item.nombreTarea)
+  setId(item.id)
+}
+
+const editarTarea = e => {
+  e.preventDefault()
+  if(!tarea.trim()){
+    console.log("el campo esta vacio")
+    setError("Escriba una tarea")
+    return
+  }
+
+  const arrayEditar = tareas.map(item => item.id === id ? {id, nombreTarea:tarea} : item)
+  setTareas(arrayEditar)
+  setModoEdicion(false)
+  setTarea("")
+  setId("")
+  setError(null)
+}
+
 
   return (
     <div className="container mt-5">
@@ -30,16 +66,44 @@ const agregarTarea = e =>{
         <div className="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la tarea</span>
-              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-              <button className="btn btn-warning btn-sm float-right">Editar</button>
-            </li>
+            {
+              tareas.length == 0 ? (
+                <li className="list-group-item">No hay Tareas</li>
+              ) : (
+                tareas.map(item => (
+
+                  <li className="list-group-item" key={item.id}>
+                    <span className="lead">{item.nombreTarea}</span>
+                    <button 
+                    className="btn btn-danger btn-sm float-right mx-2"
+                    onClick={() => eliminarTarea(item.id)}
+                    >
+                      Eliminar
+                      </button>
+      
+                    <button 
+                    className="btn btn-warning btn-sm float-right"
+                      onClick={()=> editar(item)}
+                    >Editar
+                    </button>
+                  </li>
+                    ))
+              )
+
+              
+            }
+
           </ul>
         </div>
         <div className="col-4">
-        <h4 className="text-center">Formulario</h4>
-        <form onSubmit={agregarTarea}>
+        <h4 className="text-center">
+          {
+            modoEdicion ? "Editar Tarea" : "Agregar Tarea"          }
+        </h4>
+        <form onSubmit={ modoEdicion ? editarTarea : agregarTarea}>
+          {
+            error ? <span className="text-danger">{error}</span> : null
+          }
         <input 
           type="text" 
           className="form-control mb-2"
@@ -47,7 +111,15 @@ const agregarTarea = e =>{
           onChange={e => setTarea(e.target.value)}
           value={tarea}
         />
-        <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+        { 
+        modoEdicion ? (
+          <button className="btn btn-dark btn-block" type="submit">Editar</button>
+        ) : ( 
+            <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+        )  
+        
+        }
+        
       </form>
         </div>
       </div>
